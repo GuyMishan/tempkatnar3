1d79431338c2362cefbcdb797443b8a4c61f8493
GuyMishan <gaiu01999@gmail.com>
2021-12-27 17:18:52 +0200

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,4 @@
 import React, { Fragment, Suspense, lazy } from "react";
-import { Router, Route, Switch } from "react-router-dom";
-import { connect } from "react-redux";
-import { history } from "./_helpers/history";
-import { alertActions } from "./actions/alertActions";
-import { notificationActions } from "./actions//notificationActions";
-import { PrivateRoute } from "./components/PrivateRoute";
-import "./styles/index.css";
 
 export function App() {
 
