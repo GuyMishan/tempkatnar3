41b92a7ad5067bf4116ec272a0fb1a364554d3c3
GuyMishan <gaiu01999@gmail.com>
2021-12-27 16:50:42 +0200

    first commit

--- client/src/index.js
+++ client/src/index.js
@@ -1,26 +1,24 @@
-
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-
-
-
-import Routes from './routes';
-
-import "assets/plugins/nucleo/css/nucleo.css";
-import "@fortawesome/fontawesome-free/css/all.min.css";
-import "assets/scss/argon-dashboard-react.scss";
-
-
-
-
-
-
-
+import "./index.css";
+import { App } from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import store from "./store";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
- <Routes/>,
+  // <Provider store={store}>
+    <BrowserRouter>
+      {/* <App /> */}
+    </BrowserRouter>
+  // </Provider>,
+  ,
   document.getElementById("root")
 );
 
-{/* <Route path="/auth" render={props => <AuthLayout {...props} />} /> */}
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+
+// serviceWorker.unregister();
